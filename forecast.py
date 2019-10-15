9bc0a395d7a953790f4b4d93ea06886c383630a6
AndrewSud <andrew.sudakevich@acidgreen.com.au>
2019-10-15 14:17:21 +0300

    add exception to get_forecast()

--- forecast.py
+++ forecast.py
@@ -6,20 +6,20 @@ class Forecast:
     def __init__(self, city):
         self.city = city
         self.app_id = "905db094e4f5be359d649e0ce6bfa2cd"
-        self.forecast = self.get_forecast()
 
     def get_forecast(self):
-        return requests.get(
-            "http://api.openweathermap.org/data/2.5/weather",
-            params={'q': self.city, 'units': 'metric', 'lang': 'en', 'APPID': self.app_id}
-        )
+        try:
+            return requests.get(
+                "http://api.openweathermap.org/data/2.5/weather",
+                params={'q': self.city, 'units': 'metric', 'lang': 'en', 'APPID': self.app_id}
+            )
+        except Exception as e:
+            print("Exception (weather):", e)
 
     def display_forecast(self):
-        data = self.forecast.json()
+        data = self.get_forecast().json()
         return f"City: {data['name']}\n" \
                f"Time: {datetime.today().strftime('%H:%M:%S %d-%m-%Y')}\n" \
                f"Conditions: {data['weather'][0]['description']}\n" \
                f"Temp: {data['main']['temp']}"
-
-
 
