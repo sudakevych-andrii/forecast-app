0175ac33fa4e673eb57701ef5d3cf5f4d8946adf
AndrewSud <andrew.sudakevich@acidgreen.com.au>
2020-05-21 21:19:13 +0300

    change forecast class

--- forecast.py
+++ forecast.py
@@ -1,24 +1,57 @@
 import requests
-from datetime import datetime
+from bs4 import BeautifulSoup
+import datetime
 
 
-class Forecast:
-    def __init__(self, city):
-        self.city = city
-        self.app_id = "905db094e4f5be359d649e0ce6bfa2cd"
+class WeatherForecast:
+    def __init__(self, city=None, date=datetime.datetime.now().strftime('%Y-%m-%d')):
+        self._city = city
+        self._date = date
 
-    def get_forecast(self):
+    @property
+    def city(self):
+        return self._city
+
+    @city.setter
+    def city(self, value):
+        self._city = value.lower()
+
+    @property
+    def date(self):
+        return self._date
+
+    @date.setter
+    def date(self, value):
+        self._date = value
+
+    def _get_url(self):
+        if self._city:
+            return f'https://sinoptik.ua/погода-{self._city}/{self._date}'
+        else:
+            return f'https://sinoptik.ua/'
+
+    def _get_request(self):
         try:
-            return requests.get(
-                "http://api.openweathermap.org/data/2.5/weather",
-                params={'q': self.city, 'units': 'metric', 'lang': 'en', 'APPID': self.app_id}
-            )
-        except requests.RequestException as e:
-            print("Exception (weather):", e)
+            req = requests.get(self._get_url())
+            req.raise_for_status()
+            return req
+        except requests.RequestException:
+            raise SystemExit('This city doesn\'t exist')
 
-    def display_forecast(self):
-        data = self.get_forecast().json()
-        return f"City: {data['name']}\n" \
-               f"Time: {datetime.today().strftime('%H:%M:%S %d-%m-%Y')}\n" \
-               f"Conditions: {data['weather'][0]['description']}\n" \
-               f"Temp: {data['main']['temp']}"
+    def _get_page(self):
+        page = self._get_request().text
+        return BeautifulSoup(page, 'html.parser')
+
+    def get_info(self):
+        title = self._get_page().find(class_='cityName').find('h1').text.lstrip()
+        total_info = self._get_page().find(class_='main loaded')
+        date = total_info.find(class_='date').text
+        month = total_info.find(class_='month').text
+        day = total_info.find(class_='day-link').text
+        temperature = total_info.find(class_='temperature').text.lstrip()
+        details = total_info.find(class_='weatherIco').attrs['title']
+        return f'{title} \n' \
+               f'{date} {month} \n' \
+               f'{day} \n' \
+               f'{temperature} \n' \
+               f'{details} \n'
--- telegram.py
+++ telegram.py
@@ -1,19 +1,18 @@
 import requests
 from telegram_config import config
-from forecast import Forecast
+from forecast import WeatherForecast
 
 
 class TelegramMessageSender:
-    def __init__(self, token, channel_id):
-        self.channel_id = "@" + channel_id
-        self.token = token
-        self.url = "https://api.telegram.org/bot" + self.token
-        self.method = self.url + "/sendMessage"
+    def __init__(self, channel_id, token):
+        self._channel_id = f'@{channel_id}'
+        self._token = token
+        self.__url = f'https://api.telegram.org/bot{self._token}'
+        self.__method = f'{self.__url}/sendMessage'
 
     def send_message(self, text):
         if type(text) == str and len(text):
             try:
-                requests.post(self.method, data={"chat_id": self.channel_id, "text": text})
+                requests.post(self.__method, data={"chat_id": self._channel_id, "text": text})
             except requests.RequestException as e:
                 print("Exception (send forecast):", e)
-
